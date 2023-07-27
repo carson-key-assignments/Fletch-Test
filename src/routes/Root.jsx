194c1f7e56ce0903b0c32a97288584c0012fa189
Carson-Key <keyc@oregonstate.edu>
2023-07-27 08:17:52 -0700

    Add: Helper to get unaltered ip data

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,6 +1,8 @@
 // Packages
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+// Helpers
+import ipData from '../helpers/ipData.js'
 
 function Root() {
     const { filters } = useParams();
@@ -8,6 +10,10 @@ function Root() {
     useEffect(() => {
         console.log(JSON.parse(filters))
     }, [filters])
+
+    useEffect(() => {
+        console.log(ipData)
+    }, [])
 
     return (
         <p className="text-3xl font-bold">{filters}</p>

75740aa9ba9eeee5a6cac307a399c4cfced9ef22
Carson-Key <keyc@oregonstate.edu>
2023-07-27 22:27:19 -0700

    Add: Styled spinning loader to LoadingBlocker

--- src/ui/LoadingBlocker.jsx
+++ src/ui/LoadingBlocker.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 
 function LoadingBlocker() {
     return (
-        <>
-            <div className="bg-slate-400" />
-            <div className="w-screen h-screen m-auto">Loading...</div>
-        </>
+        <div className="bg-slate-600 w-screen h-screen flex justify-center flex-col">
+            <div className="mx-auto w-fit">
+                <div className="animate-spin w-20 h-20 border-8 rounded-full border-slate-400 border-b-zinc-700" />
+            </div>
+        </div>
     );
 }
 

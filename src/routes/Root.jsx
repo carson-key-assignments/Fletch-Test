94d4528984deef098d85efb47606068a3787f62c
Carson-Key <keyc@oregonstate.edu>
2023-07-27 21:35:12 -0700

    Fix: Context Import

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,10 +1,10 @@
 // Packages
 import React, { useContext } from 'react';
 // Contexts
-import MetaDataStore from '../contexts/MetaData';
+import { MetaDataContext } from '../contexts/MetaData';
 
 function Root() {
-    const metaData = useContext(MetaDataStore)[0];
+    const metaData = useContext(MetaDataContext)[0];
 
     return <p className="text-3xl font-bold">{JSON.stringify(metaData.filters)}</p>;
 }

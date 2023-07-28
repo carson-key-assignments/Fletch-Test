e8735d72fb8f8f3473f315eba9d22ad96c3153ef
Carson-Key <keyc@oregonstate.edu>
2023-07-27 21:10:58 -0700

    Add: Set Filters reducer to MetaData context

--- src/contexts/MetaData/reducers.js
+++ src/contexts/MetaData/reducers.js
@@ -5,6 +5,8 @@ const MetaDataReducer = (state, action) => {
     switch (action.type) {
         case 'SET_INITIAL':
             return initialState;
+        case 'SET_FILTERS':
+            return { ...state, filters: action.payload }
         default:
             return state;
     }

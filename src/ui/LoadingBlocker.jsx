dbccba15825714efae5504f7d99c895bd5fe9e56
Carson-Key <keyc@oregonstate.edu>
2023-07-27 22:11:37 -0700

    Add: Non Styled loading blocker stylized component

---
+++ src/ui/LoadingBlocker.jsx
@@ -0,0 +1,13 @@
+// Packages
+import React from 'react';
+
+function LoadingBlocker() {
+    return (
+        <>
+            <div className="bg-slate-400" />
+            <div className="w-screen h-screen m-auto">Loading...</div>
+        </>
+    );
+}
+
+export default LoadingBlocker;

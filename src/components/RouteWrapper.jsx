8db9183ef3567feb4d6089d90add318ff2bf8e3c
Carson-Key <keyc@oregonstate.edu>
2023-07-27 22:12:52 -0700

    Add: DisplayLoadingBlocker to RouteWrapper so it can be used in all routes

--- src/components/RouteWrapper.jsx
+++ src/components/RouteWrapper.jsx
@@ -1,12 +1,15 @@
 // Packages
-import { useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
+// UI
+import DisplayIsLoadingBlocker from '../ui/DisplayLoadingBlocker';
 // Helpers
 import { decodeFilterHeader } from '../helpers/parseFilterHeader';
 import ipData from '../helpers/ipData';
 // Contexts
 import { MetaDataContext } from '../contexts/MetaData';
+import { IsLoadingContext } from '../contexts/IsLoading';
 
 const propTypes = {
     children: PropTypes.node.isRequired,
@@ -15,22 +18,39 @@ const propTypes = {
 function RouteWrapper({ children }) {
     const { filters } = useParams();
     const metaDataDispatch = useContext(MetaDataContext)[1];
+    const isLoadingDispatch = useContext(IsLoadingContext)[1];
 
     useEffect(() => {
+        isLoadingDispatch({
+            type: 'ADD_BLOCKER',
+            payload: 'setFiltersInMetaDataContext',
+        });
         metaDataDispatch({
             type: 'SET_FILTERS',
             payload: JSON.parse(decodeFilterHeader(filters)),
         });
+        isLoadingDispatch({
+            type: 'REMOVE_BLOCKER',
+            payload: 'setFiltersInMetaDataContext',
+        });
     }, [filters]);
 
     useEffect(() => {
+        isLoadingDispatch({
+            type: 'ADD_BLOCKER',
+            payload: 'setIpDataInMetaDataContext',
+        });
         metaDataDispatch({
             type: 'SET_IP_DATA',
             payload: ipData,
         });
+        isLoadingDispatch({
+            type: 'REMOVE_BLOCKER',
+            payload: 'setIpDataInMetaDataContext',
+        });
     }, [ipData]);
 
-    return children;
+    return <DisplayIsLoadingBlocker>{children}</DisplayIsLoadingBlocker>;
 }
 
 RouteWrapper.propTypes = propTypes;

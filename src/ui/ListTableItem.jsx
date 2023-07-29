2ce6f5b5a0765f3ae706326242859cd9317d4b3f
Carson-Key <keyc@oregonstate.edu>
2023-07-29 15:47:07 -0700

    Fix: Div in P warning

--- src/ui/ListTableItem.jsx
+++ src/ui/ListTableItem.jsx
@@ -20,12 +20,12 @@ function ListTableItem({ items, liClassName, pClassName, innerDivClassName }) {
     return (
         <li className={`flex justify-center divide-x-2 w-full h-16 px-2 py-2 ${liClassName}`}>
             {items.map((item) => (
-                <p
+                <div
                     key={item}
                     className={`flex flex-col justify-center w-1/${items.length} h-12 my-auto text-lg font-semibold text-stone-700 ${pClassName}`}
                 >
-                    <div className={`w-fit mx-auto ${innerDivClassName}`}>{item}</div>
-                </p>
+                    <p className={`w-fit mx-auto ${innerDivClassName}`}>{item}</p>
+                </div>
             ))}
         </li>
     );

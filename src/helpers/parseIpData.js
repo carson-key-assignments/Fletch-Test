b838b961bae1850cd4c56284106570fecf05123b
Carson-Key <keyc@oregonstate.edu>
2023-07-29 15:26:49 -0700

    Add: Parse ipData helper for generating pivot data struct

--- src/helpers/parseIpData.js
+++ src/helpers/parseIpData.js
@@ -9,18 +9,49 @@ export const parseUniqueIpsAndTotalBytes = (tempIpData, ip) => {
         uniqueSrc: currentSrcValue + Number(ip.result.sum_bytes),
     };
 };
+export const parseDestSrcIpData = (tempIpData, ip) => {
+    const currentDestValue = tempIpData?.[ip.result['All_Traffic.dest']]?.sources?.[ip.result['All_Traffic.src']]
+        ? tempIpData[ip.result['All_Traffic.dest']].sources[ip.result['All_Traffic.src']]
+        : 0;
+    const currentSrcValue = tempIpData?.[ip.result['All_Traffic.src']]?.destinations?.[ip.result['All_Traffic.dest']]
+        ? tempIpData[ip.result['All_Traffic.src']].sources[ip.result['All_Traffic.dest']]
+        : 0;
+
+    return {
+        ipDestBytes: currentDestValue + Number(ip.result.sum_bytes),
+        ipSrcBytes: currentSrcValue + Number(ip.result.sum_bytes),
+    };
+};
 
 export const parseIpData = (ipData) => {
     const tempUniqueIpData = {};
+    const tempIpData = {};
 
     ipData.forEach((ip) => {
         const { uniqueSrc, uniqueDest } = parseUniqueIpsAndTotalBytes(tempUniqueIpData, ip);
+        const { ipSrcBytes, ipDestBytes } = parseDestSrcIpData(tempIpData, ip);
 
-        tempUniqueIpData[ip.result['All_Traffic.dest']] = uniqueSrc;
-        tempUniqueIpData[ip.result['All_Traffic.src']] = uniqueDest;
+        tempUniqueIpData[ip.result['All_Traffic.dest']] = uniqueDest;
+        tempUniqueIpData[ip.result['All_Traffic.src']] = uniqueSrc;
+
+        if (!tempIpData[ip.result['All_Traffic.dest']]) {
+            tempIpData[ip.result['All_Traffic.dest']] = {
+                sources: {},
+                destinations: {},
+            };
+        }
+        if (!tempIpData[ip.result['All_Traffic.src']]) {
+            tempIpData[ip.result['All_Traffic.src']] = {
+                sources: {},
+                destinations: {},
+            };
+        }
+
+        tempIpData[ip.result['All_Traffic.dest']].sources[ip.result['All_Traffic.src']] = ipDestBytes;
+        tempIpData[ip.result['All_Traffic.src']].destinations[ip.result['All_Traffic.dest']] = ipSrcBytes;
     });
 
-    return { tempUniqueIpData };
+    return { tempUniqueIpData, tempIpData };
 };
 
 export default parseUniqueIpsAndTotalBytes;

76533db63e7a00c6ae8a9c890f3e42fa0a4a3f74
Carson-Key <keyc@oregonstate.edu>
2023-07-29 12:32:29 -0700

    Add: Main stylized component

---
+++ src/ui/Main.jsx
@@ -0,0 +1,22 @@
+// Packages
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+};
+const defaultProps = {
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    children: <></>,
+    className: '',
+};
+
+function Main({ children, className }) {
+    return <main className={`w-screen h-fit overflow-scroll scrollbar-hide ${className}`}>{children}</main>;
+}
+
+Main.propTypes = propTypes;
+Main.defaultProps = defaultProps;
+
+export default Main;

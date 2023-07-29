347bb7d98075d175eb0596716deeb5dc73970e22
Carson-Key <keyc@oregonstate.edu>
2023-07-29 13:02:00 -0700

    Add: Stylized Components to root route

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,11 +2,31 @@
 import React, { useContext } from 'react';
 // Contexts
 import { MetaDataContext } from '../contexts/MetaData';
+// UI
+import Header from '../ui/Header';
+import ListTable from '../ui/ListTable';
+import ListTableItem from '../ui/ListTableItem';
+import Main from '../ui/Main';
+import Page from '../ui/Page';
 
 function Root() {
     const metaData = useContext(MetaDataContext)[0];
 
-    return <p className="text-3xl font-bold">{JSON.stringify(metaData.filters)}</p>;
+    return (
+        <Page>
+            <Header>IP Addresses</Header>
+            <Main>
+                <ListTable>
+                    {metaData.ipData.map((ipData) => (
+                        <ListTableItem
+                            key={`${ipData.result['All_Traffic.src']}-${ipData.result['All_Traffic.dest']}-${ipData.result.sum_bytes}`}
+                            items={[ipData.result['All_Traffic.src']]}
+                        />
+                    ))}
+                </ListTable>
+            </Main>
+        </Page>
+    );
 }
 
 export default Root;

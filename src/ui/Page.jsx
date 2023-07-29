ff55b1d02a5046a4c0a051e35db8b997a3b75c52
Carson-Key <keyc@oregonstate.edu>
2023-07-29 12:27:04 -0700

    Add: Page stylized component

---
+++ src/ui/Page.jsx
@@ -0,0 +1,22 @@
+// Packages
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+};
+const defaultProps = {
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    children: <></>,
+    className: '',
+};
+
+function Page({ children, className }) {
+    return <div className={`w-screen h-screen overflow-scroll scrollbar-hide ${className}`}>{children}</div>;
+}
+
+Page.propTypes = propTypes;
+Page.defaultProps = defaultProps;
+
+export default Page;

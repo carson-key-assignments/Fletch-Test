786a82cfc450f60d911e87a38516daeb91ae96c7
Carson-Key <keyc@oregonstate.edu>
2023-07-30 12:08:30 -0700

    Add: No traffic text when there is no data

--- src/routes/PivotPoint.jsx
+++ src/routes/PivotPoint.jsx
@@ -1,6 +1,8 @@
 // Packages
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+// Components
+import ConditionalRender from '../components/ConditionalRender';
 // Contexts
 import { MetaDataContext } from '../contexts/MetaData';
 import { IsLoadingContext } from '../contexts/IsLoading';
@@ -59,50 +61,61 @@ function PivotPoint() {
                 {currentFilterType === 'd' ? `Traffic To: ${currentFilterIp}` : `Traffic From: ${currentFilterIp}`}
             </Header>
             <Main>
-                <ListTableHeader items={['IP Address', 'Bytes Transfered']} />
-                <ListTable>
-                    {listTabletArray.map((ip) => (
-                        <ListTableItem
-                            key={ip}
-                            items={[
-                                ip,
-                                `${formatBytes(
-                                    metaData.ipData[currentFilterIp][
-                                        currentFilterType === 'd' ? 'destinations' : 'sources'
-                                    ][ip]
-                                ).toLocaleString()}`,
-                            ]}
-                            contextMenuItems={[
-                                {
-                                    onClick: () => {
-                                        isLoadingDispatch({
-                                            type: 'ADD_OVERLAY_BLOCKER',
-                                            payload: 'addFilter',
-                                        });
-                                        metaDataDispatch({
-                                            type: 'ADD_DEST_FILTER',
-                                            payload: ip,
-                                        });
+                <ConditionalRender
+                    condition={listTabletArray.length > 0}
+                    falseReturn={
+                        <div className="flex flex-col flex-grow justify-center">
+                            <h2 className="text-bold text-2xl text-center">
+                                There is no traffic {currentFilterType === 'd' ? `to` : `from`} this IP address
+                            </h2>
+                        </div>
+                    }
+                >
+                    <ListTableHeader items={['IP Address', 'Bytes Transfered']} />
+                    <ListTable>
+                        {listTabletArray.map((ip) => (
+                            <ListTableItem
+                                key={ip}
+                                items={[
+                                    ip,
+                                    `${formatBytes(
+                                        metaData.ipData[currentFilterIp][
+                                            currentFilterType === 'd' ? 'destinations' : 'sources'
+                                        ][ip]
+                                    ).toLocaleString()}`,
+                                ]}
+                                contextMenuItems={[
+                                    {
+                                        onClick: () => {
+                                            isLoadingDispatch({
+                                                type: 'ADD_OVERLAY_BLOCKER',
+                                                payload: 'addFilter',
+                                            });
+                                            metaDataDispatch({
+                                                type: 'ADD_DEST_FILTER',
+                                                payload: ip,
+                                            });
+                                        },
+                                        jsx: 'Find Traffic To',
                                     },
-                                    jsx: 'Find Traffic To',
-                                },
-                                {
-                                    onClick: () => {
-                                        isLoadingDispatch({
-                                            type: 'ADD_OVERLAY_BLOCKER',
-                                            payload: 'addFilter',
-                                        });
-                                        metaDataDispatch({
-                                            type: 'ADD_SRC_FILTER',
-                                            payload: ip,
-                                        });
+                                    {
+                                        onClick: () => {
+                                            isLoadingDispatch({
+                                                type: 'ADD_OVERLAY_BLOCKER',
+                                                payload: 'addFilter',
+                                            });
+                                            metaDataDispatch({
+                                                type: 'ADD_SRC_FILTER',
+                                                payload: ip,
+                                            });
+                                        },
+                                        jsx: 'Find Traffic From',
                                     },
-                                    jsx: 'Find Traffic From',
-                                },
-                            ]}
-                        />
-                    ))}
-                </ListTable>
+                                ]}
+                            />
+                        ))}
+                    </ListTable>
+                </ConditionalRender>
             </Main>
         </Page>
     );

48df4a167c602fa0e3b21a2053ac34157489d124
Carson-Key <keyc@oregonstate.edu>
2023-07-29 19:23:38 -0700

    Add: Stylized Button component

---
+++ src/ui/Button.jsx
@@ -0,0 +1,34 @@
+// Packages
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+};
+const defaultProps = {
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    children: <></>,
+    className: '',
+    onClick: () => {},
+};
+
+function Button({ children, className, onClick }) {
+    return (
+        <button
+            className={`w-fit text-white bg-blue-600 rounded-3xl py-2 px-2 ${className}`}
+            type="button"
+            onClick={() => {
+                onClick();
+            }}
+        >
+            {children}
+        </button>
+    );
+}
+
+Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
+
+export default Button;

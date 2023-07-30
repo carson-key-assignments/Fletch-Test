4786e3f543dd71041cb0475bf411edabce5c8217
Carson-Key <keyc@oregonstate.edu>
2023-07-29 23:33:45 -0700

    Add: ClassName props to LoadingBlocker

--- src/ui/LoadingBlocker.jsx
+++ src/ui/LoadingBlocker.jsx
@@ -1,14 +1,31 @@
 // Packages
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function LoadingBlocker() {
+const propTypes = {
+    outerDivClassName: PropTypes.string,
+    innerDivClassName: PropTypes.string,
+    spinnerDivClassName: PropTypes.string,
+};
+const defaultProps = {
+    outerDivClassName: '',
+    innerDivClassName: '',
+    spinnerDivClassName: '',
+};
+
+function LoadingBlocker({ outerDivClassName, innerDivClassName, spinnerDivClassName }) {
     return (
-        <div className="bg-slate-600 w-screen h-screen flex justify-center flex-col">
-            <div className="mx-auto w-fit">
-                <div className="animate-spin w-20 h-20 border-8 rounded-full border-slate-400 border-b-zinc-700" />
+        <div className={`bg-slate-600 w-screen h-screen flex justify-center flex-col ${outerDivClassName}`}>
+            <div className={`mx-auto w-fit ${innerDivClassName}`}>
+                <div
+                    className={`animate-spin w-20 h-20 border-8 rounded-full border-slate-400 border-b-zinc-700 ${spinnerDivClassName}`}
+                />
             </div>
         </div>
     );
 }
+
+LoadingBlocker.propTypes = propTypes;
+LoadingBlocker.defaultProps = defaultProps;
 
 export default LoadingBlocker;

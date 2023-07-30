142104839ffc52ed4be2bb80e577b330b86e66a4
Carson-Key <keyc@oregonstate.edu>
2023-07-29 19:27:22 -0700

    Add: ContextMenu to ListTableItem

--- src/ui/ListTableItem.jsx
+++ src/ui/ListTableItem.jsx
@@ -1,32 +1,53 @@
 // Packages
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+// UI
+import ConditionalRender from '../components/ConditionalRender';
+import ContextMenu from './ContextMenu';
 
 const propTypes = {
     items: PropTypes.arrayOf(PropTypes.node),
+    contextMenuItems: PropTypes.arrayOf(PropTypes.objectOf([PropTypes.node, PropTypes.func])),
     liClassName: PropTypes.string,
     pClassName: PropTypes.string,
     innerDivClassName: PropTypes.string,
+    buttonClassName: PropTypes.string,
 };
 const defaultProps = {
     // eslint-disable-next-line react/jsx-no-useless-fragment
     items: <></>,
+    contextMenuItems: null,
     liClassName: '',
     pClassName: '',
     innerDivClassName: '',
+    buttonClassName: '',
 };
 
-function ListTableItem({ items, liClassName, pClassName, innerDivClassName }) {
+function ListTableItem({ items, liClassName, pClassName, innerDivClassName, buttonClassName, contextMenuItems }) {
+    const [openContextMenu, setOpenContextMenu] = useState(false);
+
     return (
-        <li className={`flex justify-center divide-x-2 w-full h-16 px-2 py-2 ${liClassName}`}>
-            {items.map((item) => (
-                <div
-                    key={item}
-                    className={`flex flex-col justify-center w-1/${items.length} h-12 my-auto text-lg font-semibold text-stone-700 ${pClassName}`}
-                >
-                    <p className={`w-fit mx-auto ${innerDivClassName}`}>{item}</p>
-                </div>
-            ))}
+        <li className={`w-full h-mi2-16 px-2 py-2 ${liClassName}`}>
+            <button
+                disabled={!contextMenuItems}
+                type="button"
+                onClick={() => {
+                    setOpenContextMenu((prev) => !prev);
+                }}
+                className={`flex w-full h-full justify-center divide-x-2 ${buttonClassName}`}
+            >
+                {items.map((item) => (
+                    <div
+                        key={item}
+                        className={`flex flex-col justify-center w-1/${items.length} h-12 my-auto text-lg font-semibold text-stone-700 ${pClassName}`}
+                    >
+                        <p className={`w-fit mx-auto ${innerDivClassName}`}>{item}</p>
+                    </div>
+                ))}
+            </button>
+            <ConditionalRender condition={openContextMenu && contextMenuItems}>
+                <ContextMenu items={contextMenuItems} />
+            </ConditionalRender>
         </li>
     );
 }
